32e78d023b53f7fb6b952b7917ae580d993ef2e7
sanlouise <sandra.adamshallie@gmail.com>
2017-09-30 19:58:04 -0400

    Add constants and actions

--- app/containers/HomePage/actions.js
+++ app/containers/HomePage/actions.js
@@ -1,3 +1,4 @@
+
 /*
  *
  * HomePage actions
@@ -5,11 +6,52 @@
  */
 
 import {
-  DEFAULT_ACTION,
+  UPDATE_SORT_TYPE,
+  UPDATE_LANGUAGE,
+  UPDATE_EXAMPLE,
+  UPDATE_SPACE_COMPLEXITY_WORST,
+  UPDATE_TIME_COMPLEXITY_BEST,
+  UPDATE_TIME_COMPLEXITY_WORST,
 } from './constants';
 
-export function defaultAction() {
+export function updateSortTypeAction(sortType) {
   return {
-    type: DEFAULT_ACTION,
+    type: UPDATE_SORT_TYPE,
+    sortType,
+  };
+}
+
+export function updateLanguageAction(language) {
+  return {
+    type: UPDATE_LANGUAGE,
+    language,
+  };
+}
+
+export function updateExampleAction(example) {
+  return {
+    type: UPDATE_EXAMPLE,
+    example,
+  };
+}
+
+export function updateSpaceComplexityWorstAction(spaceComplexityWorst) {
+  return {
+    type: UPDATE_SPACE_COMPLEXITY_WORST,
+    spaceComplexityWorst,
+  };
+}
+
+export function updateTimeComplexityBestAction(timeComplexityBest) {
+  return {
+    type: UPDATE_TIME_COMPLEXITY_BEST,
+    timeComplexityBest,
+  };
+}
+
+export function updateTimeComplexityWorstAction(timeComplexityWorst) {
+  return {
+    type: UPDATE_TIME_COMPLEXITY_WORST,
+    timeComplexityWorst,
   };
 }
