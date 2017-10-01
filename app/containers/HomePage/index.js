30f41145c84274cfbb4c5dbbc4cb80d97a2396c8
sanlouise <sandra.adamshallie@gmail.com>
2017-10-01 11:35:12 -0400

    Add UI, include selection options

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,51 +1,127 @@
-/**
- *
- * HomePage
- *
- */
-
+// Dependencies
 import React from 'react';
 import PropTypes from 'prop-types';
+import map from 'lodash/map';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
-
+// Externals
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
-import makeSelectHomePage from './selectors';
+// Internals
+import {
+  updateLanguageAction,
+  updateSortTypeAction,
+} from './actions';
+import {
+  selectSortType,
+  selectLanguage,
+  selectExample,
+  selectSpaceComplexityWorst,
+  selectTimeComplexityBest,
+  selectTimeComplexityWorst,
+  selectAvailableSortTypes,
+  selectAvailableLanguages,
+} from './selectors';
 import reducer from './reducer';
 import saga from './saga';
 
-export class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
+import './styles.css';
+
+
+class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const { availableLanguages, updateLanguage, availableSortTypes, updateSortType, language, sortType, example, spaceComplexityWorst, timeComplexityBest, timeComplexityWorst } = this.props;
+
     return (
       <div>
         <Helmet>
-          <title>Algorithms</title>
-          <meta name="Algorithms" content="Homepage for algorithms" />
+          <title>LangSort | Home</title>
+          <meta name="description" content="This is the homepage!" />
         </Helmet>
+
+        <div className="wrapper">
+          <div className="sidebar">
+
+            <h2>Select a Language</h2>
+
+            {map(availableLanguages, (availableLanguage) => (
+              <div key={availableLanguage} className="available-language">
+                <input
+                  onChange={(event) => updateLanguage(event.target.value)}
+                  id={availableLanguage}
+                  name="language"
+                  type="radio"
+                  value={availableLanguage}
+                />
+                <label htmlFor={availableLanguage}>{availableLanguage}</label>
+              </div>
+            ))}
+
+            <h2>Select an Algorithm</h2>
+
+            {map(availableSortTypes, (availableSortType) => (
+              <div key={availableSortType} className="available-sort-type">
+                <input
+                  onChange={(event) => updateSortType(event.target.value)}
+                  id={availableSortType}
+                  name="algorithm"
+                  type="radio"
+                  value={availableSortType}
+                />
+                <label htmlFor={availableSortType}>{availableSortType}</label>
+              </div>
+            ))}
+          </div>
+
+          <div className="algorithm-data">
+            <h1>Your selection:</h1>
+            <p>Language: <strong>{language}</strong></p>
+            <p>Sort Type: <strong>{sortType}</strong></p>
+            <p>Example: <strong>{example}</strong></p>
+            <p>Space Complexity Worst: <strong>{spaceComplexityWorst}</strong></p>
+            <p>Time Complexity Best: <code>{timeComplexityBest}</code></p>
+            <p>Time Complexity Worst: <strong>{timeComplexityWorst}</strong></p>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 HomePage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  sortType: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
+  example: PropTypes.string.isRequired,
+  spaceComplexityWorst: PropTypes.string.isRequired,
+  timeComplexityBest: PropTypes.string.isRequired,
+  timeComplexityWorst: PropTypes.string.isRequired,
+  availableSortTypes: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  availableLanguages: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  updateSortType: PropTypes.func.isRequired,
+  updateLanguage: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = createStructuredSelector({
-  homepage: makeSelectHomePage(),
+  sortType: selectSortType(),
+  language: selectLanguage(),
+  example: selectExample(),
+  spaceComplexityWorst: selectSpaceComplexityWorst(),
+  timeComplexityBest: selectTimeComplexityBest(),
+  timeComplexityWorst: selectTimeComplexityWorst(),
+  availableSortTypes: selectAvailableSortTypes(),
+  availableLanguages: selectAvailableLanguages(),
 });
 
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch,
+    updateLanguage: (language) => dispatch(updateLanguageAction(language)),
+    updateSortType: (sortType) => dispatch(updateSortTypeAction(sortType)),
   };
 }
 
 const withConnect = connect(mapStateToProps, mapDispatchToProps);
-
 const withReducer = injectReducer({ key: 'homePage', reducer });
 const withSaga = injectSaga({ key: 'homePage', saga });
 
