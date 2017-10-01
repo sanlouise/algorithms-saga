8a2d884e5df3cdc5c0480ca86792bf04372764da
sanlouise <sandra.adamshallie@gmail.com>
2017-09-30 20:26:04 -0400

    Add selectors

--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -8,18 +8,54 @@ const selectHomePageDomain = (state) => state.get('homePage');
 /**
  * Other specific selectors
  */
-
-
-/**
- * Default selector used by HomePage
- */
-
-const makeSelectHomePage = () => createSelector(
+const selectSortType = () => createSelector(
   selectHomePageDomain,
-  (substate) => substate.toJS()
+  (homePage) => homePage.get('sortType')
 );
 
-export default makeSelectHomePage;
+const selectLanguage = () => createSelector(
+  selectHomePageDomain,
+  (homePage) => homePage.get('language')
+);
+
+const selectExample = () => createSelector(
+  selectHomePageDomain,
+  (homePage) => homePage.get('example')
+);
+
+const selectSpaceComplexityWorst = () => createSelector(
+  selectHomePageDomain,
+  (homePage) => homePage.getIn(['complexities', 'space', 'worst'])
+);
+
+const selectTimeComplexityBest = () => createSelector(
+  selectHomePageDomain,
+  (homePage) => homePage.getIn(['complexities', 'time', 'best'])
+);
+
+const selectTimeComplexityWorst = () => createSelector(
+  selectHomePageDomain,
+  (homePage) => homePage.getIn(['complexities', 'time', 'worst'])
+);
+
+const selectAvailableSortTypes = () => createSelector(
+  selectHomePageDomain,
+  (homePage) => homePage.get('availableSortTypes').toJS()
+);
+
+const selectAvailableLanguages = () => createSelector(
+  selectHomePageDomain,
+  (homePage) => homePage.get('availableLanguages').toJS()
+);
+
 export {
   selectHomePageDomain,
+  selectSortType,
+  selectLanguage,
+  selectExample,
+  selectSpaceComplexityWorst,
+  selectTimeComplexityBest,
+  selectTimeComplexityWorst,
+  selectAvailableSortTypes,
+  selectAvailableLanguages,
 };
